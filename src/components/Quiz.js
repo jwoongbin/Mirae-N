757e9dd29f1b404d6ec42bb6489fd5beb66021bb
seongmin.jung <seongmin.jung@clbee.com>
2021-11-18 16:24:11 +0900

    quiz 정답처리

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,6 +2,8 @@ import './Quiz.scss';
 import exit from '../images/exit.png';
 import choco from '../images/choco.png';
 import logo from '../images/logo.png';
+import penfront from '../images/pen/penfront.png'
+import penback from '../images/pen/penback.png'
 import { BrowserRouter, Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import newdata from '../data.json'
@@ -19,7 +21,7 @@ function Quiz({data, conindex, subindex}){
   }
   
   useEffect(()=>{
-    // console.log(click,'상태 변경 감지')
+    console.log(click,'상태 변경 감지')
   },[click])
 
   const questions = questionData.map((item, index) => 
@@ -27,9 +29,20 @@ function Quiz({data, conindex, subindex}){
     <div className="contents">
       <div className="question">
         {item.question} 
-        <div className="answer" onClick={() => onClick(index)}>
-          {click[index] ? "<정답>": item.answer}
-        </div>
+      </div>
+      <div className={'answer '+(click[index] ? '' : 'on')} onClick={() => onClick(index)}>
+       
+          <img className={"answer_img"} src ={penfront} alt=''/>
+          <div className={"answer_body"}>
+            <div className="answer_text">
+              {item.answer}
+            </div>
+            <div className="answer_a">
+              {'정답'}
+            </div>
+          </div>
+          <img className={"answer_img"} src ={penback} alt=''/>
+      
       </div>
 
     </div>
@@ -41,12 +54,12 @@ function Quiz({data, conindex, subindex}){
   
     <div className="Quiz-wrap">
       <div className="header">
-        <img className="logo" src={logo} width="15%" />
+        <img className="logo" src={logo} width="15%" alt=''/>
         <div className="center">
             <img className="choco" src={choco} width= "20%" />
             <h1 className="book">{chapter}</h1>
         </div>
-        <Link to="/"><img className="exit" src={exit} /></Link>
+        <Link to="/"><img className="exit" src={exit} alt='' /></Link>
       </div>
       <div className="contents">
         <ol className="quiz">
