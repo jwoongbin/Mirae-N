2b53f6f4d5e70cf802faa06e441b5e7e79fc57e8
seongmin.jung <seongmin.jung@clbee.com>
2021-11-19 19:37:13 +0900

    추가문제 수정

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,11 +2,12 @@ import './Quiz.scss';
 import exit from '../images/exit.png';
 import choco from '../images/choco.png';
 import logo from '../images/logo.png';
-import penfront from '../images/pen/penfront.png'
-import penback from '../images/pen/penback.png'
+import penfront from '../images/pen/penfront.svg'
+import penback from '../images/pen/penback.svg'
 import { BrowserRouter, Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import newdata from '../data.json'
+import { fillParentheses } from '../common/utils';
 
 function Quiz({data, conindex, subindex}){
   const questionData = data.contents[conindex].subs[subindex].questions
@@ -24,29 +25,44 @@ function Quiz({data, conindex, subindex}){
     console.log(click,'상태 변경 감지')
   },[click])
 
-  const questions = questionData.map((item, index) => 
-  <li className="questions" key={index}> 
-    <div className="contents">
-      <div className="question">
-        {item.question} 
-      </div>
-      <div className={'answer '+(click[index] ? '' : 'on')} onClick={() => onClick(index)}>
-       
-          <img className={"answer_img"} src ={penfront} alt=''/>
-          <div className={"answer_body"}>
-            <div className="answer_text">
-              {item.answer}
+  const questions = questionData.map((item, index) => {
+    console.log(item.option_one)
+    var optionable = item.option_one != null ? true : false;
+    return(
+      <li className="questions" key={index}> 
+        <div className="num">{"0"+item.num+" "}</div>
+        <div className="contents">
+          <div className="question">
+            {fillParentheses(item.question)} 
+          </div>
+          <div className={"options" + (optionable ? "" : " off")}>
+            <div className="option">
+              {item.option_one}
             </div>
-            <div className="answer_a">
-              {'정답'}
+            <div className="option">
+              {item.option_two}
+            </div>
+            <div className="option">
+              {item.option_three}
             </div>
           </div>
-          <img className={"answer_img"} src ={penback} alt=''/>
-      
-      </div>
+          <div className={'answer '+(click[index] ? '' : 'on')} onClick={() => onClick(index)}>
+          
+              <img className={"answer_img"} src ={penfront} alt=''/>
+              <div className={"answer_body"}>
+                <div className="answer_text">
+                  {item.answer}
+                </div>
+                <div className="answer_answer">
+                  {'정답'}
+                </div>
+              </div>
+              <img className={"answer_img"} src ={penback} alt=''/>
+          
+          </div>
 
-    </div>
-  </li>)
+        </div>
+      </li>)})
   
   const chapter = (data.subject + ' ' + data.grade+'-'+data.semester);  
   console.log('newdata:', newdata);
