1118f27cd0e1badf9fca52a265938e5dbebcd63c
seongmin.jung <seongmin.jung@clbee.com>
2021-11-24 10:02:53 +0900

    액션 수정중

--- src/components/MindContent.js
+++ src/components/MindContent.js
@@ -51,11 +51,15 @@ function MindContent (content){
                     console.log("split : ", split)
                     console.log("answer_count : ", answer_count)
                     var clickindex = answer_count
-                    spanlist.push(<span className={'answer'+(boollist[clickindex] ? ' on' : '')} onClick={()=>{onClick(clickindex)}}>{split}</span>)
+                    var spantag = (
+                    <span className={'answer'+(boollist[clickindex] ? ' on' : '')} onClick={()=>{onClick(clickindex)}}>
+                        <div className={'answer_img'}>{split}</div>
+                        {split}
+                    </span>
+                    )
+                    spanlist.push(spantag)
                     console.log("answer_count : ", answer_count)
                     answer_count += 1;
-                    // temp.push(<span className={'answer'+(boollist[spanindex(index)] ? ' on' : '')} onClick={()=>{onClick(spanindex(index))}}><span className={"answer_front"}>{"1"}</span><span className={'answer_body'} >{split}</span><span className={"answer_back"}>{"1"}</span></span>)  
-                    // temp.push(<span className={'answer'+(boollist[spanindex(index)] ? ' on' : '')} onClick={()=>{onClick(spanindex(index))}}><img className={"answer_img"} src ={postfront} alt=''/><span className={'answer_body'} >{split}</span><img className={"answer_img"} src ={postback} alt=''/></span>)    
                 }
                 return true
             })
@@ -67,27 +71,6 @@ function MindContent (content){
         var linetag = <div className='line'>{spanlist}</div>;
         linelist.push(linetag)
     })
-        
-    // var splitstring = content.split(/[()]+/)
-    // if(splitstring.length%2 === 1){
-    //     splitstring.map((split, index)=>{
-    //         if(index%2 === 0){
-    //             if(split.length !== 0){
-    //                 spanlist.push(<span>{split}</span>)
-    //             }
-    //         }else{
-    //             answer_count += 1;
-    //             spanlist.push(<span className={'answer'+(boollist[spanindex(index)] ? ' on' : '')} onClick={()=>{onClick(spanindex(index))}}>{split}</span>)
-    //             // temp.push(<span className={'answer'+(boollist[spanindex(index)] ? ' on' : '')} onClick={()=>{onClick(spanindex(index))}}><span className={"answer_front"}>{"1"}</span><span className={'answer_body'} >{split}</span><span className={"answer_back"}>{"1"}</span></span>)  
-    //             // temp.push(<span className={'answer'+(boollist[spanindex(index)] ? ' on' : '')} onClick={()=>{onClick(spanindex(index))}}><img className={"answer_img"} src ={postfront} alt=''/><span className={'answer_body'} >{split}</span><img className={"answer_img"} src ={postback} alt=''/></span>)    
-    //         }
-    //         return true
-    //     })
-    // }else{
-    //     spanlist.push({data})
-    //     console.log("괄호 갯수가 이상한데...?")
-    // }
-    // console.log(spanlist);
 
     var returnvalue = <div className={'mindcontent'}>{linelist}</div>
     if(answer_count === 1 && linelist.length === 1 && span_count === 0){
@@ -99,26 +82,6 @@ function MindContent (content){
     return (
         returnvalue
     )    
-    // var boola = false
-    // var a = () =>{
-    //     boola = !boola
-    // }
-    // if(splitstring.length === 3){
-    //     console.log("스플릿 됨 : " , splitstring)
-    //     return (
-    //         <div className="contenttext">
-    //             {splitstring[0]}<span className={'answer'+(boola ? ' on' : '')} onClick={a}>{splitstring[1]}</span>{splitstring[2]}
-    //         </div>
-    //     )
-    // }else{
-    //     console.log("스플릿 안됨 : " , splitstring)
-    //     return (
-    //         <div className="contenttext">
-    //             {content}
-    //         </div>
-    //     )
-    // }
-
 }
 
 export default MindContent;
